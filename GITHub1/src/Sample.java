a15b1798a51c74e530b7b33894373421f13dbd97
vengateshgithub <venkateshduraisamy@live.com>
2019-06-21 14:37:36 +0530

    Jar files are added

--- GITHub1/src/Sample.java
+++ GITHub1/src/Sample.java
@@ -1,11 +1,23 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Sample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		System.out.println("Automation Testing");
 		System.out.println("AutoBuild progress");
+
+		System.setProperty("webdriver.chrome.driver", "G:\\chromedriver_win32\\chromedriver.exe");
+
+//		ChromeOptions opt = new ChromeOptions();
+//		opt.addArguments("---start-maximized");
+		WebDriver driver = new ChromeDriver();
+
+//driver.manage().window().maximize();
+
+		driver.get("http://secure.smartbearsoftware.com/samples/TestComplete11/WebOrders/Login.aspx");
 
 	}
 
